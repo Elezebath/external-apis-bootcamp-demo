436ff031440c7cd1abd5660e60baa6ac831134d2
Aleena C R <aleenacr1999@gmail.com>
2026-07-07 12:49:38 +0300

    Make RestClient field final in BookRestClientImpl for immutability

--- src/main/java/com/accenture/externalapis/demo/client/BookRestClientImpl.java
+++ src/main/java/com/accenture/externalapis/demo/client/BookRestClientImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Component
 public class BookRestClientImpl implements BookRestClient {
 
-    private RestClient restClient;
+    private final RestClient restClient;
     private final BookMapper bookMapper;
 
     public BookRestClientImpl(RestClient.Builder builder, ExternalServiceProperties properties) {
